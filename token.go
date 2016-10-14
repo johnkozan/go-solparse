019fc020bb3979a86218c5abd09e55a706cc99b2
John Kozan <john@kozan.sk.ca>
2016-10-14 16:38:31 -0600

    Rewrite Token to match offical implementation

--- token.go
+++ token.go
@@ -1,80 +1,371 @@
 package solparse
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Token int
 
 const (
-	// Special tokens
-	ILLEGAL Token = iota
-	EOF
-	WS
+	// End of source indicator
+	EOS Token = iota
 
-	// Literals
-	IDENT
-	STRINGLIT
-	NUMBER
+	// Punctuators
+	LParen
+	RParen
+	LBrack
+	RBrack
+	LBrace
+	RBrace
+	Colon
+	Semicolon
+	Period
+	Conditional
+	Arrow
 
-	// Misc characters
-	COMMA
-	LBRACE
-	RBRACE
-	SEMICOLON
-	LPAREN
-	RPAREN
+	// Assignment
+	Assign
+	AssignBitOr
+	AssignBitXor
+	AssignBitAnd
+	AssignShl
+	AssignSar
+	AsignShr
+	AssignAdd
+	AssignSub
+	AssignMul
+	AssignDiv
+	AssignMod
+
+	// Binary operators, sorted by precedence
+	Comma
+	Or
+	And
+	BitOr
+	BitXor
+	BitAnd
+	SHL
+	SAR
+	SHR
+	Add
+	Sub
+	Mul
+	Div
+	Mod
+	Exp
+
+	// Compare operators, sorted by precedence
+	Equal
+	NotEqual
+	LessThan
+	GreaterThan
+	LessThanOrEqual
+	GreaterThanOrEqual
+
+	// Unary operators
+	Not
+	BitNot
+	Inc
+	Dec
+	Delete
 
 	// Keywords
-	PRAGMA
-	IMPORT
-	CONTRACT
-	LIBRARY
+	Anonymous
+	As
+	Assemby
+	Break
+	Const
+	Continue
+	Contract
+	Default
+	Do
+	Else
+	Enum
+	Event
+	External
+	For
+	Function
+	Hex
+	If
+	Indexed
+	Internal
+	Import
+	Is
+	Library
+	Mapping
+	Memory
+	Modifier
+	New
+	Payable
+	Public
+	Pragma
+	Private
+	Return
+	Returns
+	Storage
+	Struct
+	Throw
+	Using
+	Var
+	While
 
-	FUNCTION
-	STRUCT
-	ENUM
-	MAPPING
-	ELEM
-	MODIFIER
-	EVENT
-	USING
-	VAR
-	BREAK
-	CONST
-	PAYABLE
-	EXTERNAL
-	PUBLIC
-	INTERNAL
-	PRIVATE
-	RETURNS
-	IF
-	WHILE
-	FOR
-	CONTINUE
-	RETURN
-	THROW
-	ASSEMBLY
+	// Ether subdenominations
+	SubWei
+	SubSzabo
+	SubFinney
+	SubEther
+	SubSecond
+	SubMinute
+	SubHour
+	SubDay
+	SubWeek
+	SubYear
+
+	// Keywords
+	Int
+	Uint
+	Bytes
+	Byte
+	String
+	Address
+	Bool
+	Fixed
+	UFixed
+	IntM
+	UIntM
+	BytesM
+	FixedMxN
+	UFixedMxN
+	TypesEnd // used as type enum end marker
+
+	// Literals
+	NullLiteral
+	TrueLiteral
+	FalseLiteral
+	Number
+	StringLiteral
+	CommentLiteral
+
+	// Identifiers
+	Identifier
+
+	// Keywords reserved for future use
+	Abstract
+	After
+	Case
+	Catch
+	Final
+	In
+	Inline
+	Interface
+	Let
+	Match
+	Of
+	Pure
+	Relocatable
+	Static
+	Switch
+	Try
+	Type
+	TypeOf
+	View
+
+	// Illegal token
+	Illegal
+
+	// Scanner internal use only
+	Whitespace
 )
 
-var tokLit = []string{"ILLEGAL", "EOF", "<whitespace>", "Identifier", "String literal", "Number", ",", "{", "}", ";", "(", ")",
-	"pragma", "import", "contract", "library", "function", "struct", "enum", "mapping", "Type", "modifier",
-	"event", "using", "var", "break", "constant", "payable", "external", "public", "internal", "private", "returns",
-	"if", "while", "for", "continue", "return", "throw", "assembly"}
+var tokenLiterals = []struct {
+	Name       string
+	Precedence int
+}{
+	{"EOS", 0},
 
-func (t Token) String() string { return tokLit[t] }
+	{"(", 0},
+	{")", 0},
+	{"[", 0},
+	{"]", 0},
+	{"{", 0},
+	{"}", 0},
+	{":", 0},
+	{";", 0},
+	{".", 0},
+	{"?", 3},
+	{"=>", 0},
 
-var visibiltySpecifiers = []Token{EXTERNAL, PUBLIC, INTERNAL, PRIVATE}
+	{"=", 2},
+	{"|=", 2},
+	{"^=", 2},
+	{"&=", 2},
+	{"<<=", 2},
+	{">>=", 2},
+	{">>>=", 2},
+	{"+=", 2},
+	{"-=", 2},
+	{"*=", 2},
+	{"/=", 2},
+	{"%=", 2},
 
-func isVisibilitySpecifier(tok Token) bool {
-	for _, v := range visibiltySpecifiers {
-		if tok == v {
-			return true
+	{",", 1},
+	{"||", 4},
+	{"&&", 5},
+	{"|", 8},
+	{"^", 9},
+	{"&", 10},
+	{"<<", 11},
+	{">>", 11},
+	{">>>", 11},
+	{"+", 12},
+	{"-", 12},
+	{"*", 13},
+	{"/", 13},
+	{"%", 13},
+	{"**", 14},
+
+	{"==", 6},
+	{"!=", 6},
+	{"<", 7},
+	{">", 7},
+	{"<=", 7},
+	{">=", 7},
+
+	{"!", 0},
+	{"~", 0},
+	{"++", 0},
+	{"--", 0},
+	{"delete", 0},
+
+	{"anonymous", 0},
+	{"as", 0},
+	{"assembly", 0},
+	{"break", 0},
+	{"const", 0},
+	{"continue", 0},
+	{"contract", 0},
+	{"default", 0},
+	{"do", 0},
+	{"else", 0},
+	{"enum", 0},
+	{"event", 0},
+	{"external", 0},
+	{"for", 0},
+	{"function", 0},
+	{"hex", 0},
+	{"if", 0},
+	{"indexed", 0},
+	{"internal", 0},
+	{"import", 0},
+	{"is", 0},
+	{"library", 0},
+	{"mapping", 0},
+	{"memory", 0},
+	{"modifier", 0},
+	{"new", 0},
+	{"payable", 0},
+	{"public", 0},
+	{"pragma", 0},
+	{"private", 0},
+	{"return", 0},
+	{"returns", 0},
+	{"storage", 0},
+	{"struct", 0},
+	{"throw", 0},
+	{"using", 0},
+	{"var", 0},
+	{"while", 0},
+
+	{"wei", 0},
+	{"szabo", 0},
+	{"finney", 0},
+	{"ether", 0},
+	{"seconds", 0},
+	{"minutes", 0},
+	{"hours", 0},
+	{"days", 0},
+	{"weeks", 0},
+	{"years", 0},
+
+	{"int", 0},
+	{"uint", 0},
+	{"bytes", 0},
+	{"byte", 0},
+	{"string", 0},
+	{"address", 0},
+	{"bool", 0},
+	{"fixed", 0},
+	{"ufixed", 0},
+	{"intM", 0},
+	{"uintM", 0},
+	{"bytesM", 0},
+	{"fixedMxN", 0},
+	{"ufixedMxN", 0},
+	{"", 0},
+
+	{"null", 0},
+	{"true", 0},
+	{"false", 0},
+	{"", 0},
+	{"", 0},
+	{"", 0},
+
+	{"", 0},
+
+	{"abstract", 0},
+	{"after", 0},
+	{"case", 0},
+	{"catch", 0},
+	{"final", 0},
+	{"in", 0},
+	{"inline", 0},
+	{"interface", 0},
+	{"let", 0},
+	{"match", 0},
+	{"of", 0},
+	{"pure", 0},
+	{"relocatable", 0},
+	{"static", 0},
+	{"switch", 0},
+	{"try", 0},
+	{"type", 0},
+	{"typeof", 0},
+	{"view", 0},
+
+	{"", 0},
+}
+
+func (t Token) String() string { return tokenLiterals[t].Name }
+
+func stringToToken(s string) (tok Token, lit string) {
+	//If the string matches a keyword then return that keyword.
+	for k, v := range tokenLiterals {
+		if v.Name == strings.ToLower(s) {
+			return Token(k), s
 		}
 	}
-	return false
+	// Otherwise return as a regular identifier.
+	return Identifier, s
+}
+
+func isVisibilitySpecifier(tok Token) bool {
+	return tok == External || tok == Public || tok == Internal || tok == Private
+}
+
+func isLocationSpecifier(tok Token) bool {
+	return tok == Memory || tok == Storage
+}
+
+func isUnaryOp(tok Token) bool {
+	return (Not <= tok && tok <= Delete) || tok == Add || tok == Sub
+}
+
+func isCountOp(tok Token) bool {
+	return tok == Inc || tok == Dec
 }
 
 func isElementaryTypeName(lit string) bool {
-	//log.Println("is vis: ", lit)
 	for _, v := range elementaryTypes {
 		if v == lit {
 			return true
